b3274c9ad68202b05699e15437cea70887f5235c
Rene Iest <rene.soto01@iest.edu.mx>
2025-10-21 17:16:36 -0600

    inicios

---
+++ Tabs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tabs
+//
+//  Created by rene on 20/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tabs/TabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabsApp.swift
+//  Tabs
+//
+//  Created by rene on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
